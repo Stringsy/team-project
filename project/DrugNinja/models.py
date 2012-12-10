9aebe7124c304fc0014f6e7586b0d330d1e85cdc
Mamas Nicolaou <mamasnicolaou@gmail.com>
2012-12-10 15:14:21 +0000

    added models to models.py and enabled admin screen

--- project/DrugNinja/models.py
+++ project/DrugNinja/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+ title = models.CharField(max_length=1000)
+ pub_date = models.DateTimeField('date published')
+
+class Question(models.Model):
+ topic = models.ForeignKey(Topic)
+ text = models.CharField(max_length=500)
+ answer = models.CharField(max_length=100)
+ 
+ class Slide(models.Model)
+ 
