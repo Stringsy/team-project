7db339b98b5a1723045d3aac6954f2695f08e072
Mamas Nicolaou <mamas.nicolaou@me.com>
2012-12-17 12:35:30 -0800

    Added collapsible (kind of) slides and questions fields to admin screen

--- project/DrugNinja/admin.py
+++ project/DrugNinja/admin.py
@@ -4,13 +4,17 @@ from django.contrib import admin
 
 class SlideInline(admin.StackedInline):
 	model = Slide
+	extra = 0
 
+class QuestionInline(admin.StackedInline):
+	model = Question
+	extra = 0
 
 class DrugAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None,		{'fields':['title']}),
 		('Date Details', {'fields':['pub_date']} )
 	]
-	inlines = [SlideInline]
+	inlines = [SlideInline, QuestionInline]
 
 admin.site.register(Topic, DrugAdmin)
